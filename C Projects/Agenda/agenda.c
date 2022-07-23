08bdc8c4045585458ca80d399c70a016ec73e924
thiagohp1 <85203313+thiagohp1@users.noreply.github.com>
2022-07-22 23:25:55 -0300

    Add files via upload

--- C Projects/Agenda/agenda.c
+++ C Projects/Agenda/agenda.c
@@ -13,24 +13,15 @@ void inicializa_evento(Evento *p, Data dt, Hora inc, Hora fim, char *d, char *l)
 }
 
 void le_evento( Evento *p ){
-        printf("Digite o dia:\n");
-        scanf("%d", &p->data.dia);
-        printf("Digite o mes:\n");
-        scanf("%d", &p->data.mes);
-        printf("Digite o ano:\n");
-        scanf("%d", &p->data.ano);
-        printf("Digite a hora de inicio:\n");
-        scanf("%d", &p->inicio.hora);
-        printf("Digite o minuto de inicio\n");
-        scanf("%d", &p->inicio.minuto);
-        printf("Digite a hora final:\n");
-        scanf("%d", &p->fim.hora);
-        printf("Digite o minuto final:\n");
-        scanf("%d", &p->fim.minuto);
-        printf("Digite a descricao:\n");
-        scanf("%s", p->descricao);
-        printf("Digite o local:\n");
-        scanf("%s", p->local);
+    le_dia(p);
+    le_mes(p);
+    le_ano(p);
+    le_hora_inicio(p);
+    le_minuto_inicio(p);
+    le_hora_final(p);
+    le_minuto_final(p);
+    le_descricao(p);
+    le_local(p);
 }
 
 void mostra_evento( Evento p ){
@@ -40,4 +31,120 @@ void mostra_evento( Evento p ){
     
 }
 
-       
+int eh_numero(int *n)
+{
+    while(scanf("%d",n) != 1)
+    {
+        while(getchar() != '\n');
+        printf("Voce inseriu uma letra\nTente outra vez com um valor valido:\n");
+    }
+    return *n;
+}
+
+void le_dia(Evento *p){
+    do {
+        printf("Digite o dia:\n");
+        eh_numero(&p->data.dia);
+         
+        if(p->data.dia > 31 || p->data.dia < 0)
+            printf("erro dia invalido\ntente outra vez\n");
+    }while(p->data.dia > 31 || p->data.dia < 0);
+    
+}
+
+void le_mes(Evento *p){
+    do {
+        printf("Digite o mes:\n");
+        eh_numero(&p->data.mes);
+
+        if(p->data.mes > 12 || p->data.mes < 0)
+            printf("erro mes invalido\ntente outra vez\n");
+    }while(p->data.mes > 12 || p->data.mes < 0);    
+}
+
+void le_ano(Evento *p){
+    do {
+        printf("Digite o ano:\n");
+        eh_numero(&p->data.ano);
+
+        if(p->data.ano > 9999 || p->data.ano < 0)
+            printf("erro ano invalido\ntente outra vez\n");
+    }while(p->data.ano > 9999 || p->data.ano < 0);
+}
+
+void le_hora_inicio(Evento *p){
+    do{
+        printf("Digite a hora de inicio:\n");
+        eh_numero(&p->inicio.hora);
+        if(p->inicio.hora > 24 || p->inicio.hora < 0)
+            printf("erro minuto invalido\ntente outra vez\n");
+    }while(p->inicio.hora > 24 || p->inicio.hora < 0);
+}
+
+void le_minuto_inicio(Evento *p){
+    do{
+        printf("Digite o minuto de inicio\n");
+        eh_numero(&p->inicio.minuto);
+
+        if(p->inicio.minuto > 59 || p->inicio.minuto < 0)
+            printf("erro minuto invalido\ntente outra vez\n");
+    }while(p->inicio.minuto > 59 || p->inicio.minuto < 0);
+
+}
+
+void le_hora_final(Evento *p){
+    do{
+        printf("Digite a hora de inicio:\n");
+        eh_numero(&p->fim.hora);
+
+        if(p->fim.hora > 24 || p->fim.hora < 0)
+            printf("erro minuto invalido\ntente outra vez\n");
+    }while(p->fim.hora > 24 || p->fim.hora < 0);
+}
+
+void le_minuto_final(Evento *p){
+    do{
+        printf("Digite o minuto de inicio\n");
+        eh_numero(&p->fim.minuto);
+
+        if(p->fim.minuto > 59 || p->fim.minuto < 0)
+            printf("erro minuto invalido\ntente outra vez\n");
+
+    }while(p->fim.minuto > 59 || p->fim.minuto < 0);
+}
+
+void le_descricao(Evento *p){
+    printf("Digite a descricao:\n");
+    scanf("%s", p->descricao);
+}
+
+void le_local(Evento *p){
+    printf("Digite o local:\n");
+    scanf("%s", p->local);
+}   
+
+int carrega_arquivo( char *nome_arq, Evento **pV, int *pN ){
+	FILE *f = fopen( nome_arq, "rt");
+	if( f == NULL ){
+		return 0; // Erro!
+	}
+	int n;
+	fscanf(f, "%d", &n);
+	Evento *v = malloc( sizeof(Evento) * n );
+	int i;
+	for( i = 0 ; i < n ; i++ ){
+		fscanf(f, "%d", &v[i].data.dia);
+        fscanf(f, "%d", &v[i].data.mes);
+        fscanf(f, "%d", &v[i].data.ano);
+        fscanf(f, "%d", &v[i].inicio.hora);
+        fscanf(f, "%d", &v[i].inicio.minuto);
+        fscanf(f, "%d", &v[i].fim.hora);
+        fscanf(f, "%d", &v[i].fim.minuto);
+		fscanf(f, " %[^\n]", v[i].descricao);
+        fscanf(f, " %[^\n]", v[i].local);
+	}
+	fclose( f );
+	*pV = v;
+	*pN = n;
+	return 1; // Sucesso!
+}
--- C Projects/Agenda/main_agenda.c
+++ C Projects/Agenda/main_agenda.c
@@ -11,25 +11,64 @@ void mostra_e( void *x ){
 	mostra_evento( *p );
 }
 
+int compara_data( void *x, void *y ){
+	Evento *a = x, *b = y;
+	
+    if( a->data.ano < b->data.ano )
+        return -1;
+        if( a->data.mes < b->data.mes )
+            return -1;
+		    if( a->data.dia < b->data.dia )
+                return -1;
+	if( a->data.ano > b->data.ano )
+        return 1;
+		if( a->data.mes > b->data.mes )
+            return 1;
+            if( a->data.dia > b->data.dia )
+                return 1;
+	return 0;
+	
+}
+
+int compara_horario( void *x, void *y ){
+	Evento *a = x, *b = y;
+	
+    if( a->inicio.hora < b->inicio.hora)
+        return 1;
+		if( a->inicio.minuto < b->inicio.minuto )
+            return 1;
+	if( a->inicio.hora > b->inicio.hora )
+        return -1;
+		if( a->inicio.minuto > b->inicio.minuto )
+            return -1;
+	return 0;
+	
+}
+
 int main(int argc, char *argv[]) {
     int op;
     Lista l1;
     Evento evento;
+    Evento *pt1 = &evento;
     
     inicializa_lista(&l1, sizeof(evento));
-
+    carrega_arquivo("teste", &pt1, &l1.tamInfo );
 
     do
     { 
         printf("\n--------\n|AGENDA|\n--------\n 1.Cadastrar um novo evento. \n 2.Mostrar todos os eventos da agenda. \n 3.Mostrar todos os eventos dessa data. \n 4.Mostrar todos os eventos que tenham essa descricao. \n 5.Remover evento.\n 6.Salvar e sair \n");
         scanf("%d", &op);
-
+        
+        int (*p)(void*, void*);
         switch (op)
         {
         case(1):
+            p = compara_data;
             le_evento( &evento);
             printf("\nEvento adicionado com sucesso!\n");
-            insere_inicio( &l1, &evento);
+            if(p == 0)
+                p = compara_horario;
+            insere_ordem( &l1, &evento, p);
             break;
 
         case(2):
@@ -49,7 +88,7 @@ int main(int argc, char *argv[]) {
             break;
 
         case(6):
-            
+            limpa_lista(&l1);
             break;
 
         default:
